3d70df0eedfb24ef25323b1640c400faf49366c7
MariiaDenysova <maria.denadel@gmail.com>
2021-11-02 09:40:15 +0200

    fix list function if records are empty

--- expense.py
+++ expense.py
@@ -38,7 +38,6 @@ def delete_expense(db_name):
 
 def list_all_expense(db_name):
     list_all_expenses = execute_query(db_name, 'SELECT * FROM expense').fetchall()
-
     if len(list_all_expenses) == 0:
         return 'You have no records'
     return f'Here is all your records:\n{list_all_expenses}'
@@ -46,22 +45,29 @@ def list_all_expense(db_name):
 def list_expenses_for_category(list_of_arguments, db_name):
     category = (list_of_arguments[2], )
     list_expenses_by_category = execute_query(db_name, 'SELECT * FROM expense WHERE category=?', category).fetchall()
-
+    if len(list_expenses_by_category) == 0:
+        return 'You have no records'
     return list_expenses_by_category
 
 def list_expenses_for_day(list_of_arguments, db_name):
     date = (list_of_arguments[3], )
     list_expenses_by_date = execute_query(db_name, 'SELECT * FROM expense WHERE date=?', date).fetchall()
+    if len(list_expenses_by_date) == 0:
+        return 'You have no records'
     return list_expenses_by_date
 
 def list_expenses_for_month(list_of_arguments, db_name):
     date = (list_of_arguments[3], )
     list_expenses_by_month = execute_query(db_name, 'SELECT * FROM expense WHERE strftime("%Y-%m", date) = ?', date).fetchall()
+    if len(list_expenses_by_month) == 0:
+        return 'You have no records'
     return list_expenses_by_month
 
 def list_expenses_for_year(list_of_arguments, db_name):
     date = (list_of_arguments[3], )
     list_expenses_by_year = execute_query(db_name, 'SELECT * FROM expense WHERE strftime("%Y", date) = ?', date).fetchall()
+    if len(list_expenses_by_year) == 0:
+        return 'You have no records'
     return list_expenses_by_year
 
 
@@ -108,8 +114,7 @@ def invoke(cli_arguments, db_name='production.db'):
             return list_all_expense(db_name)
         # python3 expense.py --help
         elif cli_arguments[1] == '--help':
-            return 'Need help?\npython3 expense.py add 100 - добавлене расхода с сумой 100 без категории и на сегодня\npython3 expense.py add 100 Еда - добавлене расхода с сумой 100 с категорией Еда на сегодня\npython3 expense.py add 100 Еда 2021-12-13 - добавлене расхода с сумой 100 с категорией Еда на 13 декабря 2021\npython3 expense.py list - статистика по всем категориям\npython3 expense.py list <name-of-the-category> - статистика по выбраной категории\npython3 expense.py list --day <day> - статистика за день / <day> в формате YYYY-MM-DD\npython3 expense.py list --month <month> - статистика за месяц  / <month> в формате YYYY-MM\npython3 expense.py list --year <year> - статистика за год / <year> в формате YYYY\npython3 expense.py delete - очистить все данные'
-
+            return 'Need help?\n python3 expense.py add 100 - добавлене расхода с сумой 100 без категории и на сегодня\n python3 expense.py add 100 Еда - добавлене расхода с сумой 100 с категорией Еда на сегодня\n python3 expense.py add 100 Еда 2021-12-13 - добавлене расхода с сумой 100 с категорией Еда на 13 декабря 2021\n python3 expense.py list - статистика по всем категориям\n python3 expense.py list <name-of-the-category> - статистика по выбраной категории\n python3 expense.py list --day <day> - статистика за день / <day> в формате YYYY-MM-DD\n python3 expense.py list --month <month> - статистика за месяц  / <month> в формате YYYY-MM\n python3 expense.py list --year <year> - статистика за год / <year> в формате YYYY\n python3 expense.py delete - очистить все данные'
 
 
 def main():
